55a8d7e0f65fb90a435ef4e63c52bfcd6a980220
Rémy Marronnier <rmarronnier@gmail.com>
2019-08-27 16:30:10 +0200

    Method_generator working

--- src/cadmium_tokenizer.cr
+++ src/cadmium_tokenizer.cr
@@ -2,15 +2,16 @@ require "cadmium_util"
 require "./cadmium/tokenizer"
 
 module Cadmium
-  # method_generator({
-  #   aggressive_tokenizer:       Cadmium::AggressiveTokenizer,
-  #   case_tokenizer:             Cadmium::CaseTokenizer,
-  #   pragmatic_tokenizer:        Cadmium::PragmaticTokenizer,
-  #   regex_tokenizer:            Cadmium::RegexTokenizer,
-  #   sentence_tokenizer:         Cadmium::SentenceTokenizer,
-  #   treebank_word_tokenizer:    Cadmium::TreebankWordTokenizer,
-  #   whitespace_tokenizer:       Cadmium::WhitespaceTokenizer,
-  #   word_punctuation_tokenizer: Cadmium::WordPunctuationTokenizer,
-  #   word_tokenizer:             Cadmium::WordTokenizer,
-  # })
+  include Util
+  method_generator({
+    aggressive_tokenizer:       Cadmium::AggressiveTokenizer,
+    case_tokenizer:             Cadmium::CaseTokenizer,
+    pragmatic_tokenizer:        Cadmium::PragmaticTokenizer,
+    regex_tokenizer:            Cadmium::RegexTokenizer,
+    sentence_tokenizer:         Cadmium::SentenceTokenizer,
+    treebank_word_tokenizer:    Cadmium::TreebankWordTokenizer,
+    whitespace_tokenizer:       Cadmium::WhitespaceTokenizer,
+    word_punctuation_tokenizer: Cadmium::WordPunctuationTokenizer,
+    word_tokenizer:             Cadmium::WordTokenizer,
+  })
 end
